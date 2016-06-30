8bc5989b05b227d668ce02a146fea810ef2c2455
Alonso Parra <alienx9889@gmail.com>
2016-06-30 18:39:12 -0400

    First intent for view

--- register/views.py
+++ register/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import  generic
+
+from .models import Professor,Course,Student
+
+class IndexView(generic.View):
+    model = Professor
+    template_name = 'register/index.html'
+
+
+
 
 # Create your views here.

03e116cea1bea2fc55d91f32d3989a50c11f7239
Alonso Parra <alienx9889@gmail.com>
2016-07-04 21:06:04 -0400

    Update: Url Added

--- profregis/urls.py
+++ profregis/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^register', include('register.urls', namespace="register")),
     url(r'^admin/', include(admin.site.urls)),
 ]
